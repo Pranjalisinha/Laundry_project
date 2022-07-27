74b01b9dd8a7a2ad8b588dfa3a1460ca566e3a9d
pranjalisinha <pranjalisinha83@gmail.com>
2022-07-27 20:26:06 +0530

    day1 updates

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.listen(process.env.PORT || 3001, ()=>{
-    console.log("Server connected succesfully");
-}, (err)=>{
-    console,log(err);
+app.listen(process.env.PORT || 3001, (err)=>{
+ if(err){
+    console.log(err)
+ } else {
+    console.log("Server connected succesfully")
+ }
 })
