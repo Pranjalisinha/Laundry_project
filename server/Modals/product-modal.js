417d702550e96b73fcb69364ecaaf0e8a99fb051
Sakshi Nayan <sakshi12princi@gmail.com>
2022-07-28 13:50:12 +0530

    day 2 create modal

--- server/Modals/product-modal.js
+++ server/Modals/product-modal.js
@@ -1,1 +1,44 @@
-"working"
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+        shirt:{
+            quantity: Number,
+            wash_type: Array,
+            price: Number 
+        },
+        t_shirt:{
+            quantity: Number,
+            wash_type: Array,
+            price: Number
+        },
+        trouser:{
+            quantity: Number,
+            wash_type: Array,
+            price: Number
+        },
+        jeans:{
+            quantity: Number,
+            wash_type: Array,
+            price: Number
+        },
+        boxer:{
+            quantity: Number,
+            wash_type: Array,
+            price: Number
+        },
+        jogger:{
+            quantity: Number,
+            wash_type: Array,
+            price: Number
+        },
+        other:{
+            quantity: Number,
+            wash_type: Array,
+            price: Number
+        }
+    
+
+})
+
+const productModal = mongoose.model("product", productSchema);
+module.exports = productModal;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,52 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const productModal = require("./Modals/product-modal");
 const app = express();
 
-app.listen(process.env.PORT || 3001, ()=>{
-    console.log("Server connected succesfully");
-}, (err)=>{
-    console,log(err);
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.listen(3001,(err)=>{
+    if(!err){
+        console.log("Server connected succesfully at 3001")
+    }
+    else{
+        console.log(err)
+    }
+});
+
+mongoose.connect("mongodb://localhost:27017/laundry",(data)=>{
+    console.log("Successfully connect to db")
+},(err)=>{
+    console.log(err)
+});
+
+app.get("/",(req,res)=>{
+    res.status(200).send("Laundry app")
+},(err)=>{
+    console.log(err)
+})
+
+app.post("/create-order",(req,res)=>{
+    console.log(req.body);
+    //res.send("post working")
+
+    productModal.create({ 
+        shirt : req.body.shirt,
+        t_shirt :req.body.t_shirt,
+        trouser : req.body.trouser,
+        jeans : req.body.jeans,
+        boxer : req.body.boxer,
+        jogger: req.body.jogger,
+        other : req.body. other
+    } ).then((data)=>{
+        res.status(200).send( data)
+            
+    }).catch((err)=>{
+        res.status(400).send(err)
+    })
+})
+
+app.get ("/list",(req,res)=>{
+
 })
