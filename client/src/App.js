b0ac6c6dbc01d527b990b5e7c62a47b01981475a
pranjalisinha <pranjalisinha83@gmail.com>
2022-07-29 15:08:07 +0530

    Header footer and sidebar created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Footer from "./components/footer";
 import Header from "./components/header";
+import Sidebar from "./components/sidebar";
 
 const App = ()=>{
   return(
     <>
     <Header/>
+    <Sidebar/>
+    <Footer/>
     </>
   )
 
---
+++ client/src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = ()=>{
+    return (
+        <>
+        <footer className="footer">
+        <p className="footer_elem"> 2022 <div className="cir">C</div> Laundry</p>
+        </footer>
+        </>
+    )
+}
+export default Footer;
