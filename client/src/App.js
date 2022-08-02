78ce385cf6d3ecafb6d22745196721fdc711c3b6
pranjalisinha <pranjalisinha83@gmail.com>
2022-08-02 23:24:44 +0530

    modified

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
 import React from "react";
 import Body from "./components/body";
-import Footer from "./components/footer";
-import Header from "./components/header";
-import Sidebar from "./components/sidebar";
 import OrderPage from "./components/order_page";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = ()=>{
   return(
     <>
-    <Header/>
     <BrowserRouter>
     <Routes>
     <Route path="/create" element={<Body/>}/>
     <Route path="/viewOrder" element={<OrderPage/>}/>
     </Routes>
     </BrowserRouter>
-    <Sidebar/>
-    <Footer/>
     </>
   )
 
